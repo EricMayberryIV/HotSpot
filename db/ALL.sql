be54c248bada8589eb8ff00cf86e3e3d41191c94
Eric A. Mayberry IV <eric@ericmayberryiv.com>
2016-03-07 19:43:08 -0500

    Updated database to address unrecognized characters in the About query.

--- db/ALL.sql
+++ db/ALL.sql
@@ -37,7 +37,7 @@ CREATE TABLE `ABOUT` (
 
 LOCK TABLES `ABOUT` WRITE;
 /*!40000 ALTER TABLE `ABOUT` DISABLE KEYS */;
-INSERT INTO `ABOUT` VALUES ('',''),('0','Stop wasting your time chatting with flaky people on your grandma’s dating app. It’s time to get out and meet people the way nature intended… While you\'re out Partying!!!!!!!!  Introducing HotSpot. The first mobile social network that connects you with real people who are going to the same places that you are going.');
+INSERT INTO `ABOUT` VALUES ('',''),('0','Stop wasting your time chatting with flaky people on your grandma\'s dating app. It\'s time to get out and meet people the way nature intended... While you\'re out Partying!!!!!!!!  Introducing HotSpot. The first mobile social network that connects you with real people who are going to the same places that you are going.');
 /*!40000 ALTER TABLE `ABOUT` ENABLE KEYS */;
 UNLOCK TABLES;
 
