38ec1809595e49a2e2f12c443a2785a4c1cb58d6
Eric A. Mayberry IV <eric@ericmayberryiv.com>
2016-03-29 00:02:44 -0400

    added error protection for sending messages with msgError.php

---
+++ msgError.php
@@ -0,0 +1,49 @@
+<?php
+  session_start(); ?><!-- required for all php files within the application -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1,
+      shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
+    <meta http-equiv="refresh" content="8;url=<?php echo $_SERVER['HTTP_REFERER'];?>" />
+
+    <!-- CSS links go here -->
+    <link rel="stylesheet" type="text/css" href="css/template.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+    <!-- End CSS links -->
+
+    <!-- Special font links go here -->
+    <!-- End font links -->
+
+    <title>HotSpot | Error Page</title>
+  </head>
+  <body>
+    <?php include("header.php"); ?>
+    <div class="container"><br/>
+      <br/>
+      <h3 class="text-center">
+        Damn, yo
+      </h3>
+      <br/>
+      <h4 class="text-center">
+        There was an error and your message didn't send.
+      </h4>
+      <br/>
+      <h5 class="text-center">Don't worry about it, I'll take you back so you
+        can try again.
+      </h5>
+    </div>
+
+    <?php include("nav.php"); ?>
+
+    <!-- The body of the page goes above this line, only scripts should
+         go below this line. -->
+
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- End JS -->
+  </body>
+</html>
--- sendmsg.php
+++ sendmsg.php
@@ -14,29 +14,30 @@
   $sql0 = "select U_ID from USER where U_USERNAME='$msgToUser';";
   $result0 = $conn->query($sql0);
   if ($result0->num_rows > 0) {
-    // output data of each row
-    while($row0 = $result0->fetch_assoc()) {
-      $msgTo = $row0["U_ID"];}
-    } else {
-    }
+  // output data of each row
+  while($row0 = $result0->fetch_assoc()) {
+    $msgTo = $row0["U_ID"];}
+  } else {
+    header('Location: msgError.php');
+  }
 
   $msgFromU = $_SESSION["login_user"];
   $sql1 = "select U_ID from USER where U_USERNAME='$msgFromU';";
   $result1 = $conn->query($sql1);
   if ($result1->num_rows > 0) {
-    // output data of each row
-    while($row1 = $result1->fetch_assoc()) {
-      $msgFrom = $row1["U_ID"];}
-    } else {
-    echo "0 results";
-    }
+  // output data of each row
+  while($row1 = $result1->fetch_assoc()) {
+    $msgFrom = $row1["U_ID"];}
+  } else {
+  header('Location: msgError.php');
+  }
 
-    $sql = "INSERT INTO dir_mess (DM_FROM_ID, DM_TO_ID, DM_SUBJECT, DM_MESSAGE,
+  $sql = "INSERT INTO dir_mess (DM_FROM_ID, DM_TO_ID, DM_SUBJECT, DM_MESSAGE,
     USER_U_ID1) VALUES ($msgFrom,$msgTo,'$msgSub','$msgBody',$msgFrom)";
 
-    if(mysqli_query($conn, $sql)){
-      header('Location: ' . $_SERVER['HTTP_REFERER']);
-    } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-    }
+  if(mysqli_query($conn, $sql)){
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+  } else {
+    header('Location: msgError.php');
+  }
 ?>
